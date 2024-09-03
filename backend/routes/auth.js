7a7f3875aff3075c20b14888cf076bb2bf9b15c1
Piyush Jaiswal <piyushjaiswal474@gmail.com>
2024-09-03 12:57:58 +0530

    login

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -57,4 +57,49 @@ router.post('/register',[
         res.status(400).json({ message: error.message });
       }
   })
+
+
+  //Register user using :post "/api/auth/register". Doesn't required Auth
+router.post('/login',[
+    body('email','Enter a valid email').isEmail(),
+    body('password','Password must be atleast 5 character').exists().isLength({min:5}),
+
+] ,async(req, res) => {
+   
+        const errors=validationResult(req);
+        if(!errors.isEmpty()){
+          const formattedErrors = errors.array().reduce((acc, error) => {
+            acc[error.path] = error.msg;
+            return acc;
+        }, {});
+        return res.status(400).json({ errors: formattedErrors });
+        }
+        const {email,password}= req.body;
+        try {
+            let user= await User.findOne({"email":email});
+        console.log({email,password,user})
+
+            if(!user){
+                return res.status(400).json({errors:{message:"No User Found"}});
+            }
+            console.log({email,password,user})
+
+            const passwordCompare=await bcrypt.compare(password,user.password);
+            if(!passwordCompare){
+                return res.status(400).json({errors:{message:"No User Found"}});
+            }
+            const payload={
+                user:{
+                    id:user.id
+                }
+            }
+            const authtoken=jwt.sign(payload,JWT_SECRET);
+            console.log(authtoken);
+            res.status(201).json({authtoken});
+      } catch (error) {
+
+        res.status(400).json(error.message);
+      }
+  })
+  
   module.exports=router;
